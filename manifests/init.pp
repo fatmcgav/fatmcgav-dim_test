2c1980dfe9b957caebe9a4446b6800d9b9ea351f
Gavin Williams <fatmcgav@gmail.com>
2017-01-10 14:47:55 +0000

    Add class param for lookup

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,13 +7,15 @@
 # [*sample_parameter*]
 #   Explanation of what this parameter affects and what it defaults to.
 #
-class dim_test {
+class dim_test (
+  $filename
+) {
 
   # Get value from hiera with module_data aswell
   $value = hiera('file_contents', 'empty')
 
   # Output value
-  file { '/tmp/hash.txt':
+  file { "/tmp/${filename}.txt":
     content => $value,
     mode    => '0644'
   }
