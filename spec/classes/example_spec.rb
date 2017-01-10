e813f0f40f1074f41b6b3e784c84d256b23695f8
Gavin Williams <fatmcgav@gmail.com>
2017-01-10 14:47:55 +0000

    Add class param for lookup

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -2,6 +2,7 @@ require 'spec_helper'
 require 'pp'
 
 hiera = Hiera.new(:config => 'spec/fixtures/hiera/hiera.yaml')
+file_name = hiera.lookup('dim_test::filename', nil, nil)
 file_contents = hiera.lookup('file_contents', nil, nil)
 
 describe 'dim_test' do
@@ -15,10 +16,11 @@ describe 'dim_test' do
 
         it { should compile.with_all_deps }
         it { pp catalogue.resources }
+        it { pp file_name }
         it { pp file_contents }
 
-        it { should contain_file('/tmp/hash.txt').with_content(/this is data-in-module file contents/) }
-        it { should contain_file('/tmp/hash.txt').with_content(file_contents) }
+        it { should contain_file("/tmp/#{file_name}.txt").with_content(/this is data-in-module file contents/) }
+        it { should contain_file("/tmp/#{file_name}.txt").with_content(file_contents) }
 
       end
     end
