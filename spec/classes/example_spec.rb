460cdce306be1c05b0531ee1acf1858a10fee66c
Gavin Williams <fatmcgav@gmail.com>
2017-01-10 14:15:00 +0000

    More spec tweaks

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -1,4 +1,5 @@
 require 'spec_helper'
+require 'pp'
 
 hiera = Hiera.new(:config => 'spec/fixtures/hiera/hiera.yaml')
 file_contents = hiera.lookup('file_contents', nil, nil)
@@ -13,8 +14,10 @@ describe 'dim_test' do
         }}
 
         it { should compile.with_all_deps }
+        it { pp catalogue.resources }
+        it { pp file_contents }
 
-        it { should contain_file('/tmp/hash.txt').with_content('this is a data-in-module file contents') }
+        it { should contain_file('/tmp/hash.txt').with_content(/this is data-in-module file contents/) }
         it { should contain_file('/tmp/hash.txt').with_content(file_contents) }
 
       end
